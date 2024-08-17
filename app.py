cf74d7e41119191f6a77f534dfe3d329bbbaeb71
Mohammad Saifi Ansari <saifiansari17@gmail.com>
2024-08-17 20:13:30 +0530

    Updated the Application

--- app.py
+++ app.py
@@ -33,6 +33,16 @@ def main():
     
     # giving a title
     st.title('Diabetes Prediction')
+    st.sidebar.title("Project Author Details")
+    st.sidebar.markdown("### Project Author :")
+    st.sidebar.info("Mohammad Saifi Ansari")
+    st.sidebar.markdown("###  Email :")
+    st.sidebar.link_button("Gmail", url="mailto: saifiansari17@gmail.com")
+    st.sidebar.markdown("###  LinkedIn :")
+    st.sidebar.link_button("LinkedIn", url="https://www.linkedin.com/in/mohammad-saifi-ansari/")
+    st.sidebar.markdown("###  Github :")
+    st.sidebar.link_button("Github", url="https://github.com/SAIFIANS")
+    # st.sidebar.button()
     
     
     # getting the input data from the user
