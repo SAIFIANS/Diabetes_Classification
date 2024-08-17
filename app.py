983f14d7ea3e13e1df538374c2bcdac2cdf3f212
Mohammad Saifi Ansari <saifiansari17@gmail.com>
2024-08-17 13:29:18 +0530

    Final Notebook Update

--- app.py
+++ app.py
@@ -1,57 +1,125 @@
-from wsgiref import simple_server
-from flask import Flask, request, app,render_template
-from flask import Response
-from flask_cors import CORS
-import pickle
-import bz2
-import numpy as np
+import streamlit as st
+import time as t
+
+# to display an image
+# st.image("IMG-20210402-CV-removebg-preview.png")
+
+# title
+st.title("Diabetes Prediction")
+
+#Header
+st.header("Machine Learning")
+
+#sub header
+st.subheader("Linear Regression")
+
+#information
+st.info("Information deatils of a user")
+
+
+#Warning Message
+st.warning("Come on time or else you will be marked absent")
+
+# write
+st.write("Employee Name")
+st.write(range(50))
+
+#Error Message
+st.error("Invalid Input")
+
+#Success Message
+st.success("Values Entered Successfully")
+
+#Markdown
+st.markdown("# Heading")
+st.markdown("## Heading")
+st.markdown("## Heading")
+st.markdown(":moon:")
+
+st.text("Any text")
+
+#to write a caption
+st.caption("Caption")
+
+# to display mathematical equation
+st.latex(r'''a+b x^2+c''')
+
+# Widgets
+
+# Checkbox
+st.checkbox("Login")
+
+
+# button
+st.button("Click")
+
+# radio widget
+st.radio("Pick you Gender", ["Male", "Female", "Other"])
+
+# select box
+st.selectbox("Pick your course", ["ML", "Cloud", "Cyber Security"])
+
+# mutli select
+st.multiselect("Choose the Department", ["Content", "Sales", "Marketing"])
+
+# selectslider
+st.select_slider("Rating", ["Bad", "Good", "Excellent", "Outstanding"])
+
+# slider
+st.slider("Enter your number", 0,30)
+
+# number_input
+st.number_input("Pick a number", 0,100)
+
+# text_input
+st.text_input("Enter your email address")
+
+# date_input
+st.date_input("Opening ceremony")
+
+# time_input
+st.time_input("Choose your slot ?")
+
+
+# text_area - to multiple text lines
+st.text_area("Welcome")
+
+# file_uploader
+st.file_uploader("Upload your resume")
+
+# color_picker
+st.color_picker("color")
+
+# progress
+st.progress(90)
+
+# spinner - to diplay a temp waiting
+with st.spinner("Processing your request"):
+    t.sleep(2)
+
+# balloons
+st.balloons()
+
+# sidebar - pins the functions/widget to the side
+st.sidebar.title("This is the sidebar")
+st.sidebar.text_input("Mail Address")
+st.sidebar.text_input("Password")
+st.sidebar.radio("Choose your profession", ["Student", "Employed", "Unemployed"])
+st.sidebar.checkbox("Save for future")
+st.sidebar.button("Submit")
+
+
+# Data Visualization
+
 import pandas as pd
-
-app = Flask(__name__)
-CORS(app)
-app.config['DEBUG'] = True
-
-scalarobject=bz2.BZ2File("model\standardScalar.pkl", "rb")
-scaler=pickle.load(scalarobject)
-modelforpred = bz2.BZ2File("model\modelForPrediction.pkl", "rb")
-model = pickle.load(modelforpred)
-
-## Route for homepage
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-## Route for Single data point prediction
-@app.route('/predictdata',methods=['GET','POST'])
-def predict_datapoint():
-    result=""
-
-    if request.method=='POST':
-
-        Pregnancies=int(request.form.get("Pregnancies"))
-        Glucose = float(request.form.get('Glucose'))
-        BloodPressure = float(request.form.get('BloodPressure'))
-        SkinThickness = float(request.form.get('SkinThickness'))
-        Insulin = float(request.form.get('Insulin'))
-        BMI = float(request.form.get('BMI'))
-        DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
-        Age = float(request.form.get('Age'))
-
-        new_data=scaler.transform([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
-        predict=model.predict(new_data)
-       
-        if predict[0] ==1 :
-            result = 'Diabetic'
-        else:
-            result ='Non-Diabetic'
-            
-        return render_template('single_prediction.html',result=result)
-
-    else:
-        return render_template('home.html')
+import numpy as np
+st.title("Bar Chart")
+data = pd.DataFrame(np.random.randn(50,2), columns=["x","y"])
+st.bar_chart(data)
+st.title("Line Chart")
+st.line_chart(data)
+st.title("Area Chart")
+st.area_chart(data)
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
-    
+
